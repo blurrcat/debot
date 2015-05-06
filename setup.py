e2e4830b28bbdd215bacafc1c2e63f3393efaaa1
blurrcat <blurrcat@gmail.com>
2015-05-06 19:10:13 +0800

    add docker support

--- setup.py
+++ setup.py
@@ -7,18 +7,6 @@ readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 
-install_requires = [
-    'Flask==0.10.1',
-    'requests==2.2.1',
-    'sarge==0.1.3',
-    'beautifulsoup4==4.3.2',
-    'gevent>=1.0'
-]
-tests_require = ['pytest==2.5.1', 'pytest-cov==1.6']
-develop_require = tests_require + [
-    'Sphinx>=1.2.1', 'pylint>=1.1.0'
-]
-
 setup(
     name='debot',
     version=__version__,
@@ -29,12 +17,7 @@ setup(
     url='https://github.com/blurrcat/debot.git',
     packages=find_packages(),
     scripts=[
-        'scripts/test_bot.py',
+        'scripts/debot-cli',
     ],
     package_dir={'debot': 'debot'},
-    install_requires=install_requires,
-    tests_require=tests_require,
-    extras_require={
-        'develop': develop_require
-    },
 )
